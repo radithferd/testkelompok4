8d1928e78faaf6f714f43372afbf8e95c735729e
meylaniic <meylaniic@gmail.com>
2025-04-16 11:44:16 +0700

    commit by amel

---
+++ test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("halo lg");
+
+        //testkelompok4
+    }
+}
--- testt.java
+++
@@ -1,6 +0,0 @@
-public class testt {
-    public static void main(String[] args) {
-        System.out.println("Halo kelompok 4");
-    }
-}
-# testkelompok4
